56b8d14a8e27acdc70c74fe4c9eae50e9b7d146b
Nidhi Navandar <73529720+navandarnidhi@users.noreply.github.com>
2025-02-20 11:43:48 +0530

    Delete Logic Building/Day 2 directory

--- Logic Building/Day 2/Assignment_1/Rectangle.java
+++
@@ -1,8 +0,0 @@
-public class Rectangle {
-    public static void main(String[] args) {
-        int l = 5; // Example length
-        int w = 3;  // Example width
-        int area = l * w;
-        System.out.println("The area of the rectangle is: " + area);
-    }
-}
--- Logic Building/Day 2/Assignment_1/Square.java
+++
@@ -1,7 +0,0 @@
-public class Square {
-    public static void main(String[] args) {
-        int s = 4; // Example side length
-        int area = s * s;
-        System.out.println("The area of the square is: " + area);
-    }
-}
