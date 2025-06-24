2a5827131b53611b06e09ca44fc6b89a69f9dd72
navandarnidhi <navandarnidhi@gmail.com>
2025-06-24 20:28:26 +0530

    24-06-2025

--- DotNet/Day7/Threading3/Program.cs
+++ DotNet/Day7/Threading3/Program.cs
@@ -1,20 +1,31 @@
-﻿namespace Threading3
+﻿namespace ThreadingExamples3
 {
     internal class Program
     {
         static void Main()
         {
-            ThreadPool.QueueUserWorkItem(new WaitCallback(Func1));
-            ThreadPool.QueueUserWorkItem(new WaitCallback(Func1), "passed value");
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(Func1));
+            //ThreadPool.QueueUserWorkItem(new WaitCallback(Func1), "passed value");
+
+            //ThreadPool.QueueUserWorkItem(Func1);
+            //ThreadPool.QueueUserWorkItem(Func1, "passed value");
+
+            int workerthreads, iothreads;
+            ThreadPool.GetAvailableThreads(out workerthreads, out iothreads);
+            ////ThreadPool.SetMinThreads()
+            ////ThreadPool.SetMaxThreads
+            //ThreadPool.GetMinThreads(out workerthreads, out iothreads);
+            //ThreadPool.GetMaxThreads(out workerthreads, out iothreads);
+            Console.WriteLine(workerthreads);
+            Console.WriteLine(iothreads);
 
             Console.ReadLine();
         }
-
         static void Func1(object obj)
         {
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Func1: " + i + " " + obj);
+                Console.WriteLine("First : " + i + " " + obj);
             }
         }
     }
---
+++ DotNet/Day8/Idisposable/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Idisposable1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Main1();  // You can also call Main2() instead
+            //Main2();
+        }
+
+        static void Main1()
+        {
+            Class1 o = new Class1();
+            o.Display();
+            o.Dispose();  // Manual dispose
+        }
+
+        static void Main2()
+        {
+            using (Class1 o = new Class1())
+            {
+                o.Display();  // Dispose is called automatically at end of using block
+            }
+        }
+    }
+
+    public class Class1 : IDisposable
+    {
+        public Class1()
+        {
+            // Open file or DB connection here
+            Console.WriteLine("Constructor called");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Display called");
+        }
+
+        public void Dispose()
+        {
+            // Close file or DB connection here
+            Console.WriteLine("Dispose code called. Write code here instead of Destructor");
+        }
+    }
+}
+
+namespace Idisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 o = new Class1();
+            o.Display();
+            o.Dispose();
+
+            try
+            {
+                o.Display();  // Will throw ObjectDisposedException
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine($"Exception caught: {ex.Message}");
+            }
+        }
+    }
+
+    public class Class1 : IDisposable
+    {
+        private bool isDisposed = false;
+
+        public Class1()
+        {
+            Console.WriteLine("Constructor called");
+        }
+
+        public void Display()
+        {
+            CheckForDisposed();
+            Console.WriteLine("Display called");
+        }
+
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                // Release resources here
+                Console.WriteLine("Dispose code called. Write code here instead of Destructor");
+                isDisposed = true;
+            }
+        }
+
+        private void CheckForDisposed()
+        {
+            if (isDisposed)
+                throw new ObjectDisposedException(nameof(Class1));
+        }
+    }
+}
